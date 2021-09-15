0e76b42dbc604221d21a5853132d4ced57766d66
Nata0110 <87442131+Nata0110@users.noreply.github.com>
2021-09-15 20:48:50 +0300

    added text Hello world not use main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include "paintwall.h"
+#define return puts("Hello world_1");return
+//#include <cstdio>
+int word = printf("Hello world_2\n");
+
+int printHelloWorld() { printf("Hello World_3\n"); return 0; }
+static int testVar = printHelloWorld();
 
 int main (int argc, char *argv[])
 {
